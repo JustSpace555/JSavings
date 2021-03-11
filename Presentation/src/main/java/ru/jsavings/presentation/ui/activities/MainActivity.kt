792f7935947f5bcbd381e0406700eda6742987ee
JustSpace555 <bigmishuk@gmail.com>
2021-03-12 01:07:51 +0300

    intro fragments and icons

--- Presentation/src/main/java/ru/jsavings/presentation/ui/activities/MainActivity.kt
+++ Presentation/src/main/java/ru/jsavings/presentation/ui/activities/MainActivity.kt
@@ -1,12 +1,16 @@
 package ru.jsavings.presentation.ui.activities
 
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 import ru.jsavings.R
 
 class MainActivity : AppCompatActivity() {
@@ -29,5 +33,15 @@ class MainActivity : AppCompatActivity() {
 		navView.background = null
 		navView.menu.getItem(2).isEnabled = false
 		supportActionBar?.hide()
+	}
+
+	override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+		val view = super.onCreateView(name, context, attrs)
+
+		listOf(bottom_app_bar, nav_view, add_transaction_floating_button).onEach {
+			it.visibility = View.INVISIBLE
+		}
+
+		return view
 	}
 }
---
+++ Presentation/src/main/java/ru/jsavings/presentation/ui/fragments/account/AddNewAccountFragment.kt
@@ -0,0 +1,6 @@
+package ru.jsavings.presentation.ui.fragments.account
+
+import ru.jsavings.presentation.ui.fragments.common.BaseFragment
+
+class AddNewAccountFragment : BaseFragment() {
+}
