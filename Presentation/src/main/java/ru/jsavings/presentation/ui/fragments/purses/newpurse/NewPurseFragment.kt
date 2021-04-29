5b75608646f6763853145ac8619a4f7839f6a07c
Михаил <m.moshkov@rabota.ru>
2021-04-29 21:10:44 +0400

    New purse fragment started

--- Presentation/src/main/java/ru/jsavings/presentation/ui/fragments/purses/newpurse/NewPurseFragment.kt
+++ Presentation/src/main/java/ru/jsavings/presentation/ui/fragments/purses/newpurse/NewPurseFragment.kt
@@ -1,6 +1,8 @@
 package ru.jsavings.presentation.ui.fragments.purses.newpurse
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -26,6 +28,18 @@ class NewPurseFragment : BaseFragment() {
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
 
+		with(bindingUtil) {
 
+			tieNewPurseName.addTextChangedListener(object : TextWatcher {
+				override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+				override fun afterTextChanged(p0: Editable?) {}
+
+				override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+					tilNewPurseName.isErrorEnabled = false
+				}
+			})
+
+			val purseTypeAdapter = Ada
+		}
 	}
 }
